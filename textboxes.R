b0deeb19c538d80e978a4efc7ada001cb2b9d815
Victoria Sass <vsass@uw.edu>
2024-06-16 22:33:02 -0700

    syllabus updates

---
+++ textboxes.R
@@ -0,0 +1,32 @@
+
+
+::: {data-id="box1" style="background: rgba(255, 255, 255, 0); border: 6px solid #e93cac; width: 150px; height: 25px; margin: 10px; position: absolute; top: 259px; left: 380px"}
+PINK border
+:::
+  
+::: {.fragment data-id="box2" style="background: #e93cac; border: 4px solid #690c48; border-radius: 15px; width: 210px; height: 140px; padding: 0px 0px 20px 10px; position: absolute; top: 10px; right: -75px"}
+Pink
+:::
+  
+::: {data-id="box2" style="background: #2ad2c9; border: 4px solid #1b8883; border-radius: 15px; width: 300px; height: 150px; padding: 0px 0px 10px 10px; position: absolute; top: 320px; left: 360px"}
+Blue
+:::
+  
+::: {.fragment fragment-index=1 data-id="box3" style="background: #aadb1e; border: 4px solid #6e8e13; border-radius: 15px; width: 200px; height: 100px; padding: 0px 10px 20px 10px; position: absolute; top: 170px; right: 70px"}
+Green
+:::
+  
+::: {.fragment fragment-index=2 data-id="box4" style="background: #ffc700; border: 4px solid #a68100; border-radius: 15px; width: 360px; height: 140px; padding: 0px 10px 20px 10px; position: absolute; top: 288px; left: 350px"}
+Yellow
+:::
+  
+::: {.fragment fragment-index=2 data-id="box5" style="background: #c5b4e3; border: 4px solid #754cbc; border-radius: 15px; width: 200px; height: 160px; padding: 0px 0px 20px 10px; position: absolute; top: 305px; right: 85px"}
+Lavender
+:::
+  
+  {{< pagebreak >}}
+
+
+  
+
+
