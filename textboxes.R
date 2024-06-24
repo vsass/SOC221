5b990bb8c0eb293af501330423f73e7d9e63d4da
Victoria Sass <vsass@uw.edu>
2024-06-23 21:28:05 -0700

    Lecture 6 finished

--- textboxes.R
+++ textboxes.R
@@ -1,26 +1,26 @@
 
 
-::: {data-id="box1" style="background: rgba(255, 255, 255, 0); border: 6px solid #e93cac; width: 150px; height: 25px; margin: 10px; position: absolute; top: 259px; left: 380px"}
+::: {data-id="box1" style="background: rgba(255, 255, 255, 0); border: 6px solid #e93cac; width: 150px; margin: 10px; position: absolute; top: 259px; left: 380px"}
 PINK border
 :::
   
-::: {.fragment data-id="box2" style="background: #e93cac; border: 4px solid #690c48; border-radius: 15px; width: 210px; height: 140px; padding: 0px 0px 20px 10px; position: absolute; top: 10px; right: -75px"}
+::: {.fragment data-id="box2" style="background: #e93cac; border: 4px solid #690c48; border-radius: 15px; width: 210px; padding: 5px 5px 5px 5px; position: absolute; top: 10px; right: -75px; text-align: center"}
 Pink
 :::
   
-::: {data-id="box2" style="background: #2ad2c9; border: 4px solid #1b8883; border-radius: 15px; width: 300px; height: 150px; padding: 0px 0px 10px 10px; position: absolute; top: 320px; left: 360px"}
+::: {data-id="box2" style="background: #2ad2c9; border: 4px solid #1b8883; border-radius: 15px; width: 300px; padding: 5px 5px 5px 5px; position: absolute; top: 320px; left: 360px; text-align: center"}
 Blue
 :::
   
-::: {.fragment fragment-index=1 data-id="box3" style="background: #aadb1e; border: 4px solid #6e8e13; border-radius: 15px; width: 200px; height: 100px; padding: 0px 10px 20px 10px; position: absolute; top: 170px; right: 70px"}
+::: {.fragment fragment-index=1 data-id="box3" style="background: #aadb1e; border: 4px solid #6e8e13; border-radius: 15px; width: 200px; padding: 5px 5px 5px 5px; position: absolute; top: 170px; right: 70px; text-align: center"}
 Green
 :::
   
-::: {.fragment fragment-index=2 data-id="box4" style="background: #ffc700; border: 4px solid #a68100; border-radius: 15px; width: 360px; height: 140px; padding: 0px 10px 20px 10px; position: absolute; top: 288px; left: 350px"}
+::: {.fragment fragment-index=2 data-id="box4" style="background: #ffc700; border: 4px solid #a68100; border-radius: 15px; width: 360px; padding: 5px 5px 5px 5px; position: absolute; top: 288px; left: 350px; text-align: center"}
 Yellow
 :::
   
-::: {.fragment fragment-index=2 data-id="box5" style="background: #c5b4e3; border: 4px solid #754cbc; border-radius: 15px; width: 200px; height: 160px; padding: 0px 0px 20px 10px; position: absolute; top: 305px; right: 85px"}
+::: {.fragment fragment-index=2 data-id="box5" style="background: #c5b4e3; border: 4px solid #754cbc; border-radius: 15px; width: 200px; padding: 5px 5px 5px 5px; position: absolute; top: 305px; right: 85px; text-align: center"}
 Lavender
 :::
   
