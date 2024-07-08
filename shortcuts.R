d36c62962c55d84830d4b9a80abdbf3595b1874a
Victoria Sass <vsass@uw.edu>
2024-07-08 06:17:31 -0700

    lecture 8

---
+++ shortcuts.R
@@ -0,0 +1,34 @@
+
+
+Pink text color
+{style="color: #e93cac"}
+
+
+
+Change text size
+::: {style="font-size: 2em"}
+
+:::
+
+  
+  
+  
+Columns
+:::: {.columns}
+
+::: {.column width="50%"}
+Left column
+:::
+  
+  ::: {.column width="50%"}
+Right column
+:::
+  
+::::
+  
+  
+Fragments
+
+::: {.fragment fragment-index=1}
+
+:::
