b4ab2e4d7a73630075dd4bb70174b72e893d7a67
Victoria Sass <vsass@uw.edu>
2024-07-16 22:34:51 -0700

    lecture 8 updates

--- shortcuts.R
+++ shortcuts.R
@@ -32,3 +32,30 @@ Fragments
 ::: {.fragment fragment-index=1}
 
 :::
+  
+  
+  
+  
+  
+  thanksgiving <- data.frame(Vegetarian = c("50 <br> $f_e = 42.67$ <br> (25.00%)", 
+                                            "72 <br> $f_e = 12.8$ <br> (36.00%)",
+                                            "78 <br> $f_e = -20.13$ <br> (39.00%)",
+                                            "200"), 
+                             Carnivore = c("110 <br> $f_e = -7.33$ <br> (20.00%)",
+                                           "150  <br> $f_e = -12.8$ <br> (27.27%)",
+                                           "290  <br> $f_e = 20.13$ <br> (52.73%)",
+                                           "550"), 
+                             TOTAL = c("160", "222", "368", "750"))
+rownames(thanksgiving) <- c("Dislikes", "Indifferrent", "Likes", "TOTAL") 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
